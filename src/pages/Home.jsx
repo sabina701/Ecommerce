c0847c58ae179b21b6df63de02a922daec70ca23
sabina701 <shresthasabeena9@gmail.com>
2026-08-03 12:46:09 +0545

    Homepage updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import axios from "axios";
-import Header from "../components/header/Header";
 import Carousel from "../components/carousel/Carousel";
 import FlashSales from "../components/flashsales/FlashSales";
-import { useEffect } from "react";
-import API from "../api/axios";
 
 const Home = () => {
   return (
